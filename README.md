13bc0be5354f6da0b3244bfd50aae6966e6fdbbc
numbp <o9o73286699@gmail.com>
2019-08-17 04:52:01 +0900

    read section to function, and readme.md

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 
 Bitflyer Realtime API Executions to mongoDB.
 
+## Supported
+go version 1.7++ with Context
+
 
 Auth: [@_numbP](https://twitte.com/_numbP)
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"time"
 
 	"gopkg.in/mgo.v2"
@@ -14,7 +14,19 @@ import (
 const (
 	DBDIAL   = "localhost:27017"
 	DBPREFIX = "test_"
+
+	LOGOUTPUTLEVEL = log.INFO
 )
+
+func init() {
+	// setup Logger
+	f, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		panic(err)
+	}
+	log.SetLevel(LOGOUTPUTLEVEL)
+	log.SetOutput(f)
+}
 
 func main() {
 	done := make(chan struct{})
@@ -25,97 +37,68 @@ func main() {
 	}
 	defer sec.Close()
 
-	btc := sec.DB(DBPREFIX + "bitflyer").C("btcjpy")
+	btc := sec.DB(DBPREFIX + "bf").C("btc")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fx := sec.DB(DBPREFIX + "bitflyer").C("btcjpy")
+	fx := sec.DB(DBPREFIX + "bf").C("fxbtc")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go func() { // 現物約定
-	Reconnect:
-		ch := make(chan jsonrpc.Response)
-		go jsonrpc.Get([]string{
-			"lightning_executions_BTC_JPY",
-		}, ch)
-		time.Sleep(time.Second)
+	// 購読チャンネルを配列で設定
+	// データベースを同数用意し各reading sectionに渡す
+	channels := []string{"lightning_executions_BTC_JPY", "lightning_executions_FX_BTC_JPY"}
+	dbs := []*mgo.Collection{btc, fx}
 
-		for {
-			select {
-			case v := <-ch:
-				switch v.Type {
-				case jsonrpc.Executions:
-					go func() {
-						for _, trade := range v.Executions {
-							if err := btc.Insert(&trade); err != nil {
-								log.Error(err)
-							}
-
-							fmt.Printf("現物: %+v\n", trade)
-						}
-					}()
-
-				case jsonrpc.Error:
-					log.Error("websocket reconnect error, ", v.Error.Error())
-					goto wsError
-
-				}
-			}
-		}
-
-	wsError:
-		close(ch)
-		time.Sleep(3 * time.Second)
-		if time.Now().Hour() == 4 { // メンテナンス時間に落ちた場合
-			log.Infof("now time %s, therefore waiting websocket reconnect 12minutes", time.Now().Format("15:04"))
-			time.Sleep(15 * time.Minute)
-			log.Info("end bitflyer mentenance time, reconnect websocket")
-		}
-		goto Reconnect
-	}()
-
-	go func() { // FX約定
-	Reconnect:
-		ch := make(chan jsonrpc.Response)
-		go jsonrpc.Get([]string{
-			"lightning_executions_FX_BTC_JPY",
-		}, ch)
-		time.Sleep(time.Second)
-
-		for {
-			select {
-			case v := <-ch:
-				switch v.Type {
-				case jsonrpc.Executions:
-					go func() {
-						for _, trade := range v.Executions {
-							if err := fx.Insert(&trade); err != nil {
-								log.Error(err)
-							}
-							fmt.Printf("FX: %+v\n", trade)
-						}
-					}()
-
-				case jsonrpc.Error:
-					log.Error("websocket reconnect error, ", v.Error.Error())
-					goto wsError
-
-				}
-			}
-		}
-
-	wsError:
-		close(ch)
-		time.Sleep(3 * time.Second)
-		if time.Now().Hour() == 4 { // メンテナンス時間に落ちた場合
-			log.Infof("now time %s, therefore waiting websocket reconnect 12minutes", time.Now().Format("15:04"))
-			time.Sleep(15 * time.Minute)
-			log.Info("end bitflyer mentenance time, reconnect websocket")
-		}
-		goto Reconnect
-	}()
+	for i, v := range channels {
+		go reading(dbs[i], v)
+	}
 
 	<-done
+}
+
+// reading section
+func reading(db *mgo.Collection, channel string) {
+Reconnect:
+	ch := make(chan jsonrpc.Response)
+	go jsonrpc.Get([]string{
+		channel,
+	}, ch)
+	time.Sleep(time.Second)
+
+	defer close(ch)
+
+	for {
+		select {
+		case v := <-ch:
+			switch v.Type {
+			case jsonrpc.Executions:
+				go func() {
+					for _, trade := range v.Executions {
+						if err := db.Insert(&trade); err != nil {
+							log.Error(err)
+						}
+
+						log.Debugf("%s: %+v\n", channel, trade)
+					}
+				}()
+
+			case jsonrpc.Error:
+				log.Error("websocket reconnect error, ", v.Error.Error())
+				goto wsError
+
+			}
+		}
+	}
+
+wsError:
+	close(ch)
+	time.Sleep(3 * time.Second)
+	if time.Now().Hour() == 4 { // メンテナンス時間に落ちた場合
+		log.Infof("now time %s, therefore waiting websocket reconnect 12minutes", time.Now().Format("15:04"))
+		time.Sleep(15 * time.Minute)
+		log.Info("end bitflyer mentenance time, reconnect websocket")
+	}
+	goto Reconnect
 }
